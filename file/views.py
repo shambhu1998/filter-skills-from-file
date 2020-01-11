dc9f1e8609576ac74a498108e4e9a5fe0ce51793
shambhu1998 <sksr140@gmail.com>
2020-01-11 14:45:04 +0545

    Added camelot to convert pdf to csv.

--- file/views.py
+++ file/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
-import subprocess
+import camelot
+# import subprocess
 # from django.core.files import File
 import os, datetime
 
@@ -22,13 +23,20 @@ def index(request):
         filename = fss.save(rename, upload_file)
         upload_file_path = fss.path(filename)
 
-        output = open('output.txt', 'w')
-        subprocess.call(['pdf2txt.py', upload_file_path], stdout=output)
-        output.close()
+        # output = open('output.txt', 'w')
+        # subprocess.call(['pdf2txt.py', upload_file_path], stdout=output)
+        # output.close()
+        # os.remove(upload_file_path)
+
+        tables = camelot.read_pdf(upload_file_path, flavor='stream', table_areas=['0,800,800,0'])
+        # tables.export('foo.csv', f='csv', compress=True)
+        tables[0].to_csv('foo.csv')
         os.remove(upload_file_path)
 
         return render(request, 'file/index.html', {
             'upload_file_path': upload_file_path
         })
+
+
     else:
         return render(request, 'file/index.html')
